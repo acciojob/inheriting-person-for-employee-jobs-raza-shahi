c13cec78266ec140e1a637f6a1f61e706d3c057f
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-02-04 22:53:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // complete this js code
 class Person{
 	constructor(name,age){
-		this.name;
-		this.age;
+		this.name = name;
+		this.age = age;
 	}
 	greet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
